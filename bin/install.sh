e515f3e81aacdf44b258f0dfb2123444145db7b7
maciek01 <maciek@kolesnik.org>
2023-11-12 22:16:14 -0500

    system setup tweaks

--- bin/install.sh
+++ bin/install.sh
@@ -1,14 +1,13 @@
 
 
 sudo apt install -y python3-pip
-sudo apt install -y proj-bin
 
 sudo python3 -m pip cache purge
 
 sudo pip install --upgrade pip
 
-sudo pip3 install --upgrade --no-cache-dir mavsdk
-sudo pip3 install --upgrade --no-cache-dir httplib2 requests pyserial
-sudo pip3 install --upgrade --no-cache-dir pyproj
+sudo pip install --upgrade --no-cache-dir mavsdk
+sudo pip install --upgrade --no-cache-dir httplib2 requests pyserial
+sudo pip install --upgrade --no-cache-dir pyproj
 
 
