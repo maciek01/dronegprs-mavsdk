7214bfb46dd5bc41b44910eaf49bf71aec76968e
maciek01 <maciek@kolesnik.org>
2023-11-12 21:12:34 -0500

    instaler optimization

--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,14 @@
 
 
 sudo apt install -y python3-pip
+sudo apt install -y proj-bin
 
-python3 -m pip cache purge
+sudo python3 -m pip cache purge
+
+sudo pip install --upgrade pip
 
 sudo pip3 install --upgrade --no-cache-dir mavsdk
-sudo pip3 install --upgrade --no-cache-dir httplib2 requests pyserial pyproj
+sudo pip3 install --upgrade --no-cache-dir httplib2 requests pyserial
+sudo pip3 install --upgrade --no-cache-dir pyproj
 
 
